712469d927d307d3edaf5003904137571d613154
Aivynn <aivyn1337@gmail.com>
2022-08-14 02:31:47 +0300

    removed irrational classes

--- src/main/java/com/service/CustomerService.java
+++ src/main/java/com/service/CustomerService.java
@@ -10,6 +10,6 @@ public class CustomerService {
     public static Customer createCustomer() {
         Random random = new Random();
         int age = random.nextInt(15,110);
-        return new Customer("vasek@gmail.com",age);
+        return new Customer("somemail@gmail.com",age);
     }
 }
